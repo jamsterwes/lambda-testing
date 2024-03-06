0530f45b37092f93ec899623f29c89cde808c6db
Wesley Taylor <jamsterwes@gmail.com>
2024-03-06 13:09:27 -0600

    fully implemented index.mjs

--- pickup_selection/main.go
+++ pickup_selection/main.go
@@ -107,17 +107,97 @@ func getStreetGeometry(radius float64, latitude float64, longitude float64) [][]
 		log.Fatal(err)
 	}
 
+	// Unpack each street's line segments
+	// elements: Street[]
+	// Street = []{ lat: number, lon: number }
 	for _, street := range v.GetArray("elements") {
 		var streetGeometry [][2]float64
-		for _, coords := range street.GetArray() {
+		for _, coords := range street.GetArray("geometry") {
 			streetGeometry = append(streetGeometry, [2]float64{
 				coords.GetFloat64("lat"),
 				coords.GetFloat64("lon"),
 			})
 		}
+		geometries = append(geometries, streetGeometry)
 	}
 
 	return geometries
+}
+
+// Returns [][lat, long]
+func intersectLineRing(cx float64, cy float64, a float64, b float64, x1 float64, y1 float64, x2 float64, y2 float64) [][2]float64 {
+	// Step 1. Set origin + scale
+	X1 := (x1 - cx) / a
+	X2 := (x2 - cx) / a
+	Y1 := (y1 - cy) / b
+	Y2 := (y2 - cy) / b
+
+	// Step 2. Solve
+	// Step 2a. Get quadratic coefficients
+	A := math.Pow(X2 - X1, 2.0) + math.Pow(Y2 - Y1, 2.0)
+	B := 2.0 * ( X1 * (X2 - X1) + Y1 * (Y2 - Y1) )
+	C := X1 * X1 + Y1 * Y1 - 1.0
+
+	// Step 2b. Find discriminant
+	D := B * B - 4.0 * A * C
+
+	// Step 2c. Get # of solutions
+	var ts []float64
+	if (D < 0) {
+		return [][2]float64{}
+	} else if (D > 0) {
+		ts = append(ts,
+			(-B + math.Sqrt(D)) / (2 * A),
+			(-B - math.Sqrt(D)) / (2 * A),
+		)
+	} else {
+		ts = append(ts, -B / (2.0 * A))
+	}
+
+	// Step 3. Convert t values into coords
+	var solutions [][2]float64
+	for _, t := range ts {
+		// Ignore t outside (0, 1)
+		if (t < 0 || t > 1) {
+			continue
+		}
+
+		// Get coords
+		solutions = append(solutions, [2]float64{
+			(x2 - x1) * t + x1,
+			(y2 - y1) * t + y1,
+		})
+	}
+
+	return solutions
+}
+
+// Returns [][lat, long]
+func intersectWayRing(wayGeom [][2]float64, radius float64, latitude float64, longitude float64) [][2]float64 {
+	// Step 0. Ignore empty geom
+	if len(wayGeom) == 0 {
+		return [][2]float64{}
+	}
+
+	// Step 1. Get the two radii of the ellipse
+	a := milesToDegLongitude(radius, latitude)
+	b := milesToDegLatitude(radius, latitude)
+
+	// Step 2. Accumulate points
+	var points [][2]float64
+	for i, _ := range wayGeom[:len(wayGeom) - 1] {
+		// Get line segment
+		y1 := wayGeom[i][0]
+		x1 := wayGeom[i][1]
+		y2 := wayGeom[i+1][0]
+		x2 := wayGeom[i+1][1]
+
+		// Get solutions and concat
+		solutions := intersectLineRing(longitude, latitude, a, b, x1, y1, x2, y2)
+		points = append(points, solutions...)
+	}
+
+	return points
 }
 
 type MyEvent struct {
@@ -130,16 +210,24 @@ func HandleRequest(ctx context.Context, event *MyEvent) (*string, error) {
 		return nil, fmt.Errorf("received nil event")
 	}
 
-	fmt.Printf("Received event: %f, %f\n", event.Latitude, event.Longitude)
-
-	// Get the bounding box (1mi x 1mi) centered at user position
-	left, bottom, right, top := getUserBoundingBox(1, event.Latitude, event.Longitude)
-
 	// Get the street geometry in a 1mi x 1mi box centered at user position
-	getStreetGeometry(1, event.Latitude, event.Longitude)
+	var points [][2]float64
+	streetGeometries := getStreetGeometry(1, event.Latitude, event.Longitude)
 
-	// For now, return the bounding box as JSON
-	response := fmt.Sprintf("{\"left\": %f, \"bottom\": %f, \"right\": %f, \"top\": %f}", left, bottom, right, top)
+	for _, radius := range []float64{0.1, 0.25, 0.5, 0.75} {
+		for _, streetGeom := range streetGeometries {
+			solutions := intersectWayRing(streetGeom, radius, event.Latitude, event.Longitude)
+			points = append(points, solutions...)
+		}
+	}
+
+	// Now convert points to string
+	var response string = `{"points": [`
+	for _, point := range points {
+		response += fmt.Sprintf(`{"lat": %f, "long": %f}`, point[0], point[1])
+	}
+	response += `],"pointCount": ` + fmt.Sprintf(`%d`, len(points)) + `}`
+
 	return &response, nil
 }
 

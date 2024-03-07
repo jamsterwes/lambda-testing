5bf521004e0eae95141510c37bcf7270c39efead
Wes Taylor <jamsterwes@gmail.com>
2024-03-07 17:52:24 -0600

    moved ring radii to its own variable

--- pickup_selection/main.go
+++ pickup_selection/main.go
@@ -12,6 +12,8 @@ type PickupSelectionResponse struct {
 	PointCount int        `json:"pointCount"`
 }
 
+var RING_RADII []float64 = []float64{0.1, 0.25, 0.5, 0.75}
+
 func HandleRequest(ctx context.Context, event *Location) (*PickupSelectionResponse, error) {
 	if event == nil {
 		return nil, fmt.Errorf("received nil event")
@@ -22,7 +24,7 @@ func HandleRequest(ctx context.Context, event *Location) (*PickupSelectionRespon
 	streetGeometries := getStreetGeometry(1, *event)
 
 	// Loop through 4 preset radii to find the intersecting points
-	for _, radius := range []float64{0.1, 0.25, 0.5, 0.75} {
+	for _, radius := range RING_RADII {
 		// For this specific radius, find the intersecting points
 		// and append them to the points slice
 		for _, streetGeom := range streetGeometries {
